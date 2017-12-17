51b2842c7a59298fd5eb8405d651df9bb070e7c6
lucas <none@none>
2017-12-17 17:23:12 -0500

    Added a levelselectMenu and added a level.

--- core/src/com/fcfruit/zombiesmash/stages/LevelSelectStage.java
+++ core/src/com/fcfruit/zombiesmash/stages/LevelSelectStage.java
@@ -1,26 +1,38 @@
 package com.fcfruit.zombiesmash.stages;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.ai.btree.utils.DistributionAdapters;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageTextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextArea;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.fcfruit.zombiesmash.Environment;
 import com.fcfruit.zombiesmash.level.NightLevel;
 import com.fcfruit.zombiesmash.physics.Physics;
 import com.fcfruit.zombiesmash.screens.GameScreen;
+import com.fcfruit.zombiesmash.ui.CustomImageButton;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by Lucas on 2017-12-16.
@@ -28,43 +40,54 @@ import java.util.ArrayList;
 
 public class LevelSelectStage extends Stage {
 
-    Table table;
+    ArrayList<CustomImageButton> buttons =new ArrayList<CustomImageButton>();
+
+    SpriteBatch spriteBatch = new SpriteBatch();
 
     public LevelSelectStage(Viewport v){
         super(v);
 
-        table = new Table();
-        table.setWidth(getWidth());
-        table.align(Align.left | Align.top);
-        table.setPosition(0, getHeight());
-
-        Skin skin = new Skin(Gdx.files.internal("gui/defaultSkin/uiskin.json"));
-
-        TextButton button;
-        for(int i = 0; i < 2; i++){
-            button = new TextButton(""+(i+1), skin);
-            button.setUserObject(i + 1);
-            button.addListener(new ClickListener(){
-                @Override
-                public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-                    onLevelSelect((Integer) ((TextButton)event.getListenerActor()).getUserObject());
-                    return super.touchDown(event, x, y, pointer, button);
-                }
-            });
-            table.add(button).width(getWidth()/2).height(getHeight());
+        CustomImageButton customImageButton;
+        int row = 0;
+        int column = 0;
+        for(int i = 0; i < Gdx.files.internal("maps/night_map/levels").list().length; i++){
+            if(column > 9) {
+                row += 1;
+                column = 0;
+            }
+            customImageButton = new CustomImageButton(new Texture(Gdx.files.internal("gui/level_select/level_box.png")), ""+(i+1));
+            customImageButton.setUserData(i+1);
+            customImageButton.setPosition(column * customImageButton.getWidth(), (getHeight() - customImageButton.getHeight()) - (customImageButton.getHeight() * row));
+            column += 1;
+            buttons.add(customImageButton);
         }
 
-        addActor(table);
+
 
     }
 
     @Override
     public void draw() {
+        spriteBatch.setProjectionMatrix(getCamera().combined);
+        spriteBatch.begin();
+        for(CustomImageButton c : buttons){
+            c.draw(spriteBatch);
+        }
+        spriteBatch.end();
         super.draw();
     }
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        Vector3 pos = getCamera().unproject(new Vector3(screenX, screenY, 0));
+        for(CustomImageButton c : buttons){
+            if(c.getBoundingRectangle().contains(pos.x, pos.y)){
+                c.touchDown();
+                onLevelSelect((Integer)c.getUserData());
+                Gdx.app.log("a", ""+c.getUserData());
+                break;
+            }
+        }
         return super.touchDown(screenX, screenY, pointer, button);
     }
 
