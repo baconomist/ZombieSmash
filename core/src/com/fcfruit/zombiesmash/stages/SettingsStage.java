2e6b5c6d6670d7e3a340f41fcf2696ee83b1f679
lucas <none@none>
2017-12-17 17:23:48 -0500

    Added settings stage

---
+++ core/src/com/fcfruit/zombiesmash/stages/SettingsStage.java
@@ -0,0 +1,10 @@
+package com.fcfruit.zombiesmash.stages;
+
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+/**
+ * Created by Lucas on 2017-12-17.
+ */
+
+public class SettingsStage extends Stage {
+}
