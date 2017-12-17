97812c8c21e0a64982b4d1253582f272d8918a15
lucas <none@none>
2017-12-17 17:23:37 -0500

    Added customImageButton

---
+++ core/src/com/fcfruit/zombiesmash/ui/CustomImageButton.java
@@ -0,0 +1,56 @@
+package com.fcfruit.zombiesmash.ui;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.fcfruit.zombiesmash.Environment;
+import com.fcfruit.zombiesmash.level.Objective;
+
+/**
+ * Created by Lucas on 2017-12-17.
+ */
+
+public class CustomImageButton extends Sprite {
+
+    BitmapFont font = new BitmapFont(Gdx.files.internal("gui/defaultSkin/default.fnt"));
+
+    String text;
+
+    Object userData;
+
+    public Texture imageDown;
+
+    public CustomImageButton(Texture t, String s){
+        super(t);
+
+        text = s;
+
+
+
+    }
+
+    public void draw(SpriteBatch batch){
+        super.draw(batch);
+        font.draw(batch, text, this.getX() + this.getWidth()/2, this.getY() + this.getHeight()/2);
+        Gdx.app.log("thois", ""+this.getY() + " " + this.getBoundingRectangle().getY());
+    }
+
+    public void setUserData(Object object){
+        userData = object;
+    }
+
+    public Object getUserData(){
+        return userData;
+    }
+
+    public void touchDown(){
+        if(imageDown!=null){
+            this.setTexture(imageDown);
+        }
+    }
+
+
+
+}
