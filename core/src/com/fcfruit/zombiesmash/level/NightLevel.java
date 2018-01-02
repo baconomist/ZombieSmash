3643227855ff09e6679a1f0eebf2d0c315ba9b50
lucas <none@none>
2018-01-02 14:25:22 -0500

    Added new map

--- core/src/com/fcfruit/zombiesmash/level/NightLevel.java
+++ core/src/com/fcfruit/zombiesmash/level/NightLevel.java
@@ -3,6 +3,7 @@ package com.fcfruit.zombiesmash.level;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.utils.Json;
 import com.badlogic.gdx.utils.JsonReader;
@@ -30,6 +31,8 @@ public class NightLevel extends Level{
 
 
         this.sprite = new Sprite(new Texture(Gdx.files.internal("maps/night_map/night_map_new.png")));
+        // Move to the left to show only playable map
+        this.sprite.setPosition(-956, this.sprite.getY());
 
         this.objective = new House();
         this.objective.setPosition(7f, 0f);
