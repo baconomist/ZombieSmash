3804ceaac1870d0c46d69e14b0fd18e32937a3b9
lucas <none@none>
2018-01-02 14:42:53 -0500

    Made light position more reliable.

--- core/src/com/fcfruit/zombiesmash/physics/Lighting.java
+++ core/src/com/fcfruit/zombiesmash/physics/Lighting.java
@@ -2,6 +2,7 @@ package com.fcfruit.zombiesmash.physics;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.box2d.World;
 import com.fcfruit.zombiesmash.Environment;
 
@@ -30,7 +31,9 @@ public class Lighting {
             rayHandler = new RayHandler(world);
             rayHandler.setAmbientLight(0f, 0f, 0f, 0.7f);
             lights = new ArrayList<Light>();
-            lights.add(new PointLight(rayHandler, Environment.settings.getLightIntensity(), Color.WHITE, 2.5f, 5, 4.7f));
+            Vector3 pos = Environment.physicsCamera.unproject(Environment.gameCamera.project(new Vector3(1050, 902.4f, 0)));
+            lights.add(new PointLight(rayHandler, Environment.settings.getLightIntensity(), Color.WHITE, 2.5f, pos.x, Environment.physicsCamera.viewportHeight - pos.y));
+
         }
     }
 
