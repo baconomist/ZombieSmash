d1a5d4958350d6f87f0c7146621a2c456a7307dd
lucas <none@none>
2018-05-21 15:31:40 -0400

    Rename Flags.java -> Config.java

--- core/src/com/fcfruit/zombiesmash/Config.java
+++ core/src/com/fcfruit/zombiesmash/Config.java
@@ -4,9 +4,9 @@ package com.fcfruit.zombiesmash;
  * Created by Lucas on 2018-05-05.
  */
 
-public class Flags
+public class Config
 {
     public static boolean DEBUG = true;
     public static boolean DEBUG_PHYSICS = false;
-    public static boolean DEBUG_POLYGONS = true;
+    public static boolean DEBUG_POLYGONS = false;
 }
