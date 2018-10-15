0a751b44014312f07bc4311f4ca405a43848bde9
Lucas Borowiecki <none@none>
2018-10-15 18:41:03 -0400

    Added animated splashscreen

--- core/src/com/fcfruit/zombiesmash/screens/SplashScreen.java
+++ core/src/com/fcfruit/zombiesmash/screens/SplashScreen.java
@@ -2,29 +2,46 @@ package com.fcfruit.zombiesmash.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.fcfruit.zombiesmash.Environment;
-import com.fcfruit.zombiesmash.ZombieSmash;
+import com.fcfruit.zombiesmash.GifDecoder;
 
 public class SplashScreen implements Screen
 {
     private StretchViewport viewport;
+    private Animation<TextureRegion> animation;
 
-    Sprite sprite;
-    SpriteBatch spriteBatch;
-    float alpha = 0.5f;
+    private Sprite logo;
+    private Sprite background;
+    private Sprite text;
+
+    private SpriteBatch spriteBatch;
+    private float alpha = 0.5f;
+    private float elapsed;
 
     public SplashScreen()
     {
         viewport = new StretchViewport(1920, 1080);
 
-        this.sprite = new Sprite(new Texture(Gdx.files.internal("ui/splashscreen.png")));
-        this.sprite.setAlpha(alpha);
-
         this.spriteBatch = new SpriteBatch();
+
+        animation = GifDecoder.loadGIFAnimation(Animation.PlayMode.NORMAL, Gdx.files.internal("ui/splashscreen/splashscreen.gif").read());
+
+        logo = new Sprite();
+
+        text = new Sprite(new Texture(Gdx.files.internal("ui/splashscreen/text.png")));
+        text.setAlpha(0);
+
+        Pixmap pix = new Pixmap(1, 1, Pixmap.Format.RGBA8888);
+        pix.drawPixel(0, 0, 0xffffffff); // Blank background, 1px
+        background = new Sprite(new Texture(pix));
+        background.setSize(this.viewport.getWorldWidth(), this.viewport.getWorldHeight());
     }
 
     @Override
@@ -35,9 +52,7 @@ public class SplashScreen implements Screen
     @Override
     public void render(float delta)
     {
-
-        if(this.alpha <= 1f)
-            this.sprite.setAlpha(this.alpha);
+        this.elapsed += delta;
 
         if(Environment.assets.update() && this.alpha >= 1.5f) // When loading finished
         {
@@ -48,9 +63,20 @@ public class SplashScreen implements Screen
 
         this.spriteBatch.setProjectionMatrix(viewport.getCamera().combined);
 
+        logo.setRegion(this.animation.getKeyFrame(this.elapsed));
+        logo.setSize(logo.getRegionWidth(), logo.getRegionHeight());
+        logo.setPosition(this.viewport.getWorldWidth()/2 - logo.getRegionWidth()/2, this.viewport.getWorldHeight()/2 - 250);
+
+        text.setPosition(500, 100);
+        if(this.alpha < 1f)
+            text.setAlpha(this.alpha);
+
         this.spriteBatch.begin();
-        this.sprite.draw(this.spriteBatch);
+            background.draw(spriteBatch);
+            logo.draw(spriteBatch);
+            text.draw(spriteBatch);
         this.spriteBatch.end();
+
         this.alpha += delta/10f;
     }
 
@@ -83,6 +109,6 @@ public class SplashScreen implements Screen
     @Override
     public void dispose()
     {
-
+        this.spriteBatch.dispose();
     }
 }
