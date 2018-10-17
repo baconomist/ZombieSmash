c35889b3ea08ac3432b3ca2be4a00d78d9deb786
Lucas Borowiecki <none@none>
2018-10-16 22:24:31 -0400

    - Added python script for renaming exported splashscreen files - Working logo animation!!!!!!
    Tips:
    when exporting logo atlas, choose the "bleed" option in spine to remove borders
    gpu can't handle 16k images****

--- core/src/com/fcfruit/zombiesmash/screens/SplashScreen.java
+++ core/src/com/fcfruit/zombiesmash/screens/SplashScreen.java
@@ -7,15 +7,20 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.fcfruit.zombiesmash.Environment;
-import com.fcfruit.zombiesmash.GifDecoder;
+
+import java.util.Comparator;
 
 public class SplashScreen implements Screen
 {
     private StretchViewport viewport;
-    private Animation<TextureRegion> animation;
+
+    private Animation<TextureAtlas.AtlasRegion> animation;
+    private float frame_time = 40; // milliseconds
 
     private Sprite logo;
     private Sprite background;
@@ -31,7 +36,21 @@ public class SplashScreen implements Screen
 
         this.spriteBatch = new SpriteBatch();
 
-        animation = GifDecoder.loadGIFAnimation(Animation.PlayMode.NORMAL, Gdx.files.internal("ui/splashscreen/splashscreen.gif").read());
+        // This atlas needs multiple images as the GPU on some android devices can't load a 16k image, just turns black instead.
+        TextureAtlas textureAtlas = new TextureAtlas(Gdx.files.internal("ui/splashscreen/logo_anim.atlas"));
+        Array<TextureAtlas.AtlasRegion> atlasRegions = textureAtlas.getRegions();
+        atlasRegions.sort(new Comparator<TextureAtlas.AtlasRegion>()
+        {
+            @Override
+            public int compare(TextureAtlas.AtlasRegion o1, TextureAtlas.AtlasRegion o2)
+            {
+                return Integer.valueOf(o1.name) - Integer.valueOf(o2.name);
+            }
+        });
+
+        animation = new Animation<TextureAtlas.AtlasRegion>(frame_time, atlasRegions.toArray());
+        animation.setPlayMode(Animation.PlayMode.NORMAL);
+        animation.setFrameDuration(frame_time/1000);
 
         logo = new Sprite();
 
@@ -52,7 +71,8 @@ public class SplashScreen implements Screen
     @Override
     public void render(float delta)
     {
-        this.elapsed += delta;
+        delta = Math.min(delta, 0.1f);
+        this.elapsed += delta/2;
 
         if(Environment.assets.update() && this.alpha >= 1.5f) // When loading finished
         {
@@ -65,7 +85,7 @@ public class SplashScreen implements Screen
 
         logo.setRegion(this.animation.getKeyFrame(this.elapsed));
         logo.setSize(logo.getRegionWidth(), logo.getRegionHeight());
-        logo.setPosition(this.viewport.getWorldWidth()/2 - logo.getRegionWidth()/2, this.viewport.getWorldHeight()/2 - 250);
+        logo.setPosition(this.viewport.getWorldWidth()/2 - logo.getWidth()/2, this.viewport.getWorldHeight()/2 - logo.getHeight()/2);
 
         text.setPosition(500, 100);
         if(this.alpha < 1f)
